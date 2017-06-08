c5397b6d39dd1948532cc5dc6c3427c8289d5ad8
Lars Swemmer <larsswemmer@gmail.com>
2017-06-08 16:45:03 +0200

    SpotifyAuth: finish v1

--- app/Http/Controllers/SpotifyAuthController.php
+++ app/Http/Controllers/SpotifyAuthController.php
@@ -2,17 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\SpotifyAuth;
 use Illuminate\Http\Request;
+use SpotifyWebAPI;
 
 class SpotifyAuthController extends Controller
 {
     public function index()
     {
-    	return view('spotifyauth.index');
+    	// auth credentials
+		$session = new SpotifyWebAPI\Session(
+		    env('SPOTIFY_CLIENT_ID'),
+		    env('SPOTIFY_CLIENT_SECRET'),
+		    env('SPOTIFY_REDIRECT_URI')
+		);
+
+		$options = [
+		    'scope' => [
+		        'user-follow-read',
+		        'user-follow-modify',
+		        'playlist-modify-public',
+		        'user-top-read',
+		        'user-read-private',
+		    ],
+		];
+
+		$oauthUrl = $session->getAuthorizeUrl($options);
+		$spotifyAuth = SpotifyAuth::first();
+
+    	return view('spotifyauth.index', compact('oauthUrl', 'spotifyAuth'));
     }
 
     public function callback()
     {
+    	$session = new SpotifyWebAPI\Session(env('SPOTIFY_CLIENT_ID'), env('SPOTIFY_CLIENT_SECRET'), env('SPOTIFY_REDIRECT_URI'));
+		$session->requestAccessToken($_GET['code']);
+		$accessToken = $session->getAccessToken();
+
+		$api = new SpotifyWebAPI\SpotifyWebAPI();
+		$api->setAccessToken($accessToken);
+
+		$spotifyAuth = SpotifyAuth::updateOrCreate(
+		    ['user_id' => auth()->id()],
+		    ['accessToken' => $session->getAccessToken(), 'refreshToken' => $session->getRefreshToken()]
+		);
+
     	return redirect('spotify-auth');
     }
 }
--- resources/views/spotifyauth/index.blade.php
+++ resources/views/spotifyauth/index.blade.php
@@ -6,9 +6,16 @@
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
                 <div class="panel-heading">Spotify Authentication</div>
-
                 <div class="panel-body">
-                    <a href="#" class="btn btn-lg btn-pill btn-primary">Authenticate with Spotify</a>
+                    @if(!empty($spotifyAuth))
+                        <a href="{{ $oauthUrl }}" class="btn btn-lg btn-pill btn-success disabled"><span class="icon icon-check"></span> Connected to Spotify</a>
+
+                        <div style="margin-top: 10px;">
+                            <a href="{{ $oauthUrl }}" class="text-muted">Connect again...</a>
+                        </div>
+                    @else
+                        <a href="{{ $oauthUrl }}" class="btn btn-lg btn-pill btn-primary">Log in to Spotify</a>
+                    @endif
                 </div>
             </div>
         </div>
